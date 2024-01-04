62e26397898d271c5d5e30f9603103ac118cc399
fluorjo <fluorjo@naver.com>
2024-01-05 08:42:55 +0900

    login, register ui

--- src/app/auth/register/page.tsx
+++ src/app/auth/register/page.tsx
@@ -2,12 +2,11 @@
 import Button from "@/components/Button";
 import Input from "@/components/Input";
 import axios from "axios";
-import { signIn } from "next-auth/react";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import React, { useState } from "react";
 import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
-const LoginPage = () => {
+const RegisterPage = () => {
   const [isLoading, setIsLoading] = useState(false);
   const router = useRouter();
   const {
@@ -16,6 +15,7 @@ const LoginPage = () => {
     formState: { errors },
   } = useForm<FieldValues>({
     defaultValues: {
+      name: "",
       email: "",
       password: "",
     },
@@ -24,7 +24,7 @@ const LoginPage = () => {
   const onSubmit: SubmitHandler<FieldValues> = async (body) => {
     setIsLoading(true);
     try {
-      const data = signIn("credentials", body);
+      const { data } = await axios.post("/api/register", body);
       console.log(data);
       router.push("/auth/login");
     } catch (error) {
@@ -41,7 +41,7 @@ const LoginPage = () => {
         action=""
         onSubmit={handleSubmit(onSubmit)}
       >
-        <h1 className={"text-2xl"}>Login</h1>
+        <h1 className={"text-2xl"}>Register</h1>
         <Input
           id="email"
           label="Email"
@@ -50,7 +50,14 @@ const LoginPage = () => {
           errors={errors}
           required
         ></Input>
-
+        <Input
+          id="name"
+          label="Name"
+          disabled={isLoading}
+          register={register}
+          errors={errors}
+          required
+        ></Input>
         <Input
           id="password"
           label="Password"
@@ -64,13 +71,10 @@ const LoginPage = () => {
         <Button label="Register" />
         <div className={"text-center"}>
           <p className={"text-gray-400"}>
-            Not a member?{" "}
-            <Link
-              href={"/auth/register"}
-              className={"text-black hover:underline"}
-            >
+            Already a member?{" "}
+            <Link href={"/auth/login"} className={"text-black hover:underline"}>
               {" "}
-              Register{" "}
+              Login{" "}
             </Link>
           </p>
         </div>
@@ -79,4 +83,4 @@ const LoginPage = () => {
   );
 };
 
-export default LoginPage;
+export default RegisterPage;
